ad3c2d3016093cf985f4c614441a98e410e250fd
Y28628 <yuya.620kouki416@icloud.com>
2024-05-17 21:28:47 +0900

    levtech

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -20,7 +21,7 @@ public function index(Post $post)//インポートしたPostをインスタン�
 {
     return view('posts.create');
 }
-public function store(Request $request, Post $post)
+public function store(PostRequest $request, Post $post)
 {
   $input = $request['post'];
   $post->fill($input)->save();
